0ac7c66eedb7b56339520b82dea76a0344dd6974
Kunal Shantilal <kshantilal03@gmail.com>
2017-09-04 18:48:35 +1200

    successfully get data from a blog using the blogID

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,33 @@
+var accessToken;
+
 function getAccessToken(){
 	$.ajax({
 		url: "config/config.json",
 		dataType: "json",
 		success: function(DataFromJSON){
-			console.log(DataFromJSON);
+			console.log(DataFromJSON.AccessToken);
+			accessToken = DataFromJSON.AccessToken;
+			getId();
 		},
 		error: function(){
 			console.log("Something went wrong");
+		}
+	});
+
+}
+
+function getId(){
+	$.ajax({
+		url: "https://www.googleapis.com/blogger/v3/blogs/1665758190571819430?key="+accessToken,
+		dataType:"jsonp",
+		success:function(DataFromBlogger){
+			console.log(DataFromBlogger)
+
+		},
+
+
+		error:function(){
+		 console.log("Something went wrong");
 		}
 	});
 
