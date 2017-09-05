56be52019d2e29a8e1ddcff70c1c3f1ce6c2e0b2
Kunal Shantilal <kshantilal03@gmail.com>
2017-09-05 18:01:51 +1200

    login working but only through console url

--- public/js/script.js
+++ public/js/script.js
@@ -1,62 +1,6 @@
-var apiKey;
+var ApiKey,clientIdJSON,clientSecret;
+
 var allKeys = [];
-
-var discoveryDocs = ["https://people.googleapis.com/$discovery/rest?version=v1"];
-var clientId = '863978693509-mrknbvl68d4vpusg82gsuncjgsgqk7td.apps.googleusercontent.com';
-
-var scopes = 'profile';
-
-function handleClientLoad() {
-	// Load the API client and auth2 library
-	gapi.load('client:auth2', initClient);
-}
-function initClient() {
-	gapi.client.init({
-		apiKey: apiKey,
-		discoveryDocs: discoveryDocs,
-		clientId: clientId,
-		scope: scopes
-	}).then(function () {
-	  // Listen for sign-in state changes.
-	  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-	  // Handle the initial sign-in state.
-	  updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-	  authorizeButton.onclick = handleAuthClick;
-	  signoutButton.onclick = handleSignoutClick;
-	});
-}
-function updateSigninStatus(isSignedIn) {
-	if (isSignedIn) {
-	  authorizeButton.style.display = 'none';
-	  signoutButton.style.display = 'block';
-	  makeApiCall();
-	} else {
-	  authorizeButton.style.display = 'block';
-	  signoutButton.style.display = 'none';
-	}
-}
-
-function handleAuthClick(event) {
-	gapi.auth2.getAuthInstance().signIn();
-}
-
-function handleSignoutClick(event) {
-	gapi.auth2.getAuthInstance().signOut();
-}
-// Load the API and make an API call.  Display the results on the screen.
-function makeApiCall() {
-	gapi.client.people.people.get({
-	  'resourceName': 'people/me',
-	  'requestMask.includeField': 'person.names'
-	}).then(function(resp) {
-	  var p = document.createElement('p');
-	  var name = resp.result.names[0].givenName;
-	  p.appendChild(document.createTextNode('Hello, '+name+'!'));
-	  document.getElementById('content').appendChild(p);
-	});
-}
-
-
 
 function getAccessToken(){
 	$.ajax({
@@ -67,12 +11,17 @@ function getAccessToken(){
 
 			allKeys.push({
 				apiKey: DataFromJSON.apiKey,
-				clientID: DataFromJSON.ClientID,
-				clientSecret: DataFromJSON.ClientSecret
+				clientId: DataFromJSON.ClientID,
+				clientSec: DataFromJSON.ClientSecret
 				
 			})
-			console.log(allKeys[0].apiKey);
-			apiKey = allKeys[0].apiKey;
+			//All these variables are named at the top
+			ApiKey = allKeys[0].apiKey;
+			clientIdJSON = allKeys[0].clientId;
+			clientSecret = allKeys[0].clientSec;
+			
+			
+			//Runs the function getId
 			getId();
 		},
 		error: function(){
@@ -84,7 +33,7 @@ function getAccessToken(){
 
 function getId(){
 	$.ajax({
-		url: "https://www.googleapis.com/blogger/v3/blogs/4002601360602175877/posts?key="+apiKey, //need a apikey
+		url: "https://www.googleapis.com/blogger/v3/blogs/4002601360602175877/posts?key="+ApiKey, //need a apikey
 		dataType:"jsonp",
 		success:function(DataFromBlogger){
 			console.log(DataFromBlogger)
@@ -101,3 +50,74 @@ function getId(){
 }
 
 getAccessToken();
+
+
+//Google authentication stuff
+var discoveryDocs = ["https://people.googleapis.com/$discovery/rest?version=v1"];
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
+var scopes = "profile";
+
+handleClientLoad();
+
+function handleClientLoad(){
+  // Loading the Google Client JavaScript Authentication Library
+  gapi.load('client:auth2', initClient);
+}
+
+function initClient(){
+  // Using the config keys to access the api
+  gapi.client.init({
+      apiKey: ApiKey,
+      discoveryDocs: discoveryDocs,
+      clientId: clientIdJSON,
+      scope: scopes
+  }).then(function (){
+    // Checking whether user is signed in
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handling the existing sign-in state, in case user is already signed in.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
+    // Click function for sign in/sign out buttons
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  });
+}
+
+function updateSigninStatus(isSignedIn){
+  // If user is signed in, show/hide authentication sign buttons
+  if (isSignedIn) {
+    // Hide sign in, show sign out.
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    makeApiCall();
+  } else {
+    // Hide sign out, show sign in.
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
+}
+
+// This is where authentication is called
+function handleAuthClick(event){
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+// This is the sign out function
+function handleSignoutClick(event){
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+// Load the API and make an API call.  Display the results on the screen.
+function makeApiCall(){
+  gapi.client.people.people.get({
+    'resourceName': 'people/me',
+    'requestMask.includeField': 'person.names'
+  }).then(function(resp) {
+    var p = document.createElement('p');
+    var name = resp.result.names[0].givenName;
+    p.appendChild(document.createTextNode('Hello, '+name+'!'));
+    document.getElementById('content').appendChild(p);
+  });
+}
